208c4cc09f7421a71476a795820b3b6a2f04696c
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-07-04 12:44:23 -0700

    Add sign out link

--- spec/features/member_account_spec.rb
+++ spec/features/member_account_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'member manages account' do
   let!(:member) { FactoryGirl.create(:member) }
-  
+
   scenario 'by signing in' do
 
     visit root_path
@@ -15,5 +15,18 @@ feature 'member manages account' do
     fill_in 'member[password]', with: member.password
     click_button 'Log in'
     expect(page).to have_content 'Signed in successfully.'
+  end
+
+  scenario 'by signing out' do
+    login_as(member, scope: :member)
+
+    visit root_path
+
+    expect(page).to have_content 'Sign out'
+
+    click_link 'Sign out'
+
+    expect(page).to have_content 'Sign in'
+    expect(page).to have_content 'Signed out successfully.'
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,66 +38,9 @@ RSpec.configure do |config|
 	  end
 	end
 
-	config.infer_spec_type_from_file_location!
-
-	config.filter_rails_from_backtrace!
-
-end
-
-#Replace with below for Option 2: with 'selenium-webdriver'
-	#Note: if selenium-webdriver gem does not update to newest Firefox version fast enough for you
-	#you can download a specific version of Firefox and configure to use that version in tests
-	#example:
-		#Capybara.register_driver :selenium do |app|
-			#require 'selenium/webdriver'
-			#Selenium::WebDriver::Firefox::Binary.path = "/home/user/Sites/firefox46/firefox"
-			#Capybara::Selenium::Driver.new(app, :browser => :firefox)
-		#end
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-# Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'spec_helper'
-require 'rspec/rails'
-require 'capybara/rspec'
-require 'simple_bdd'
-require 'shoulda/matchers'
-
-ActiveRecord::Migration.maintain_test_schema!
-
-RSpec.configure do |config|
-
-	config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-	config.use_transactional_fixtures = false
-
-	config.before(:suite) do
-	  DatabaseCleaner.clean_with(:truncation)
-	end
-
-	config.before(:each) do
-	  DatabaseCleaner.strategy = :transaction
-	end
-
-	config.before(:each, :js => true) do
-	  DatabaseCleaner.strategy = :truncation
-	end
-
-	config.before(:each) do
-	  DatabaseCleaner.start
-	end
-
-	config.after(:each) do
-	  DatabaseCleaner.clean
-	end
-
-	config.include SimpleBdd, type: :feature
-
-	Shoulda::Matchers.configure do |config|
-	  config.integrate do |with|
-	    with.test_framework :rspec
-	    with.library :rails
-	  end
+	config.include Warden::Test::Helpers
+	config.before :suite do
+		Warden.test_mode!
 	end
 
 	config.infer_spec_type_from_file_location!
